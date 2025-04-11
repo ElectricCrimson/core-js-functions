9ff15695298b53d0f79323f578ec9d7b2af77682
ElectricCrimson <synthetic0aesthetic@gmail.com>
2025-04-11 13:52:33 +0300

    feat: solve one task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -137,8 +137,20 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const getFunc = func();
+
+  const memoizedFunc = () => {
+    const cache = [];
+
+    if (cache.length < 1) {
+      cache.push(getFunc);
+    }
+
+    return cache[0];
+  };
+
+  return memoizedFunc;
 }
 
 /**
